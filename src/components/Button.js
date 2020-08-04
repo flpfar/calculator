f06d5a20bc8de64d173c4d79ad68eb96d010ebea
Felipe Rosa <flp.far@gmail.com>
2020-08-04 17:27:23 -0300

    Add handleClick and prop to ButtonPanel and Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Button({ name, color, wide }) {
+function Button({
+  name, color, wide, clickHandler,
+}) {
   const width = wide ? '50%' : '25%';
+
+  function handleClick(buttonName) {
+    return clickHandler(buttonName);
+  }
+
   return (
-    <button type="button" className="btn" style={{ ...color ? { backgroundColor: color } : null, width }}>
+    <button type="button" className="btn" onClick={() => handleClick(name)} style={{ ...color ? { backgroundColor: color } : null, width }}>
       { name }
     </button>
   );
@@ -14,11 +21,13 @@ Button.propTypes = {
   name: PropTypes.string.isRequired,
   color: PropTypes.string,
   wide: PropTypes.bool,
+  clickHandler: PropTypes.func,
 };
 
 Button.defaultProps = {
   color: 'orange',
   wide: false,
+  clickHandler: null,
 };
 
 export default Button;
